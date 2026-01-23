9c183db7efc8e3e2aaa6067855cccbe8a78796b5
HooWC <wengchin123456@gmail.com>
2026-01-23 10:37:48 +0800

    Hoo Testing Example

--- SpaceHSG/Controllers/HomeController.cs
+++ SpaceHSG/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
+using System.IO;  // Ensure this reference is included
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using SpaceHSG.Models;
 
@@ -6,27 +8,53 @@ namespace SpaceHSG.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private string basePath = @"C:\Hoo_Note\sharehsg";
 
         public IActionResult Index()
         {
-            return View();
+            try
+            {
+                // Get folder and file list
+                var directories = System.IO.Directory.GetDirectories(basePath)
+                    .Select(path => new {
+                        Name = new System.IO.DirectoryInfo(path).Name,
+                        Type = "Folder",
+                        Size = 0L,  // Add Size property
+                        Modified = System.IO.Directory.GetLastWriteTime(path),
+                        Extension = ""
+                    });
+
+                var files = System.IO.Directory.GetFiles(basePath)
+                    .Select(path => new {
+                        Name = System.IO.Path.GetFileName(path),
+                        Type = "File",
+                        Size = new System.IO.FileInfo(path).Length,
+                        Modified = System.IO.File.GetLastWriteTime(path),
+                        Extension = System.IO.Path.GetExtension(path)
+                    });
+
+                // Merge display - now both anonymous types have the same structure
+                ViewBag.Items = directories.Concat(files);
+                ViewBag.BasePath = basePath;
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View();
+            }
         }
 
-        public IActionResult Privacy()
+        // Download file
+        public IActionResult Download(string filename)
         {
-            return View();
-        }
+            var filePath = Path.Combine(basePath, filename);
+            if (!System.IO.File.Exists(filePath))
+                return NotFound();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "application/octet-stream", filename);
         }
     }
 }
