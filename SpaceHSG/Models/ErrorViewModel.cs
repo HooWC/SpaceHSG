e85bfc959ccb2499d31abb981d852b92d96a8802
HooWC <wengchin123456@gmail.com>
2026-01-23 10:06:00 +0800

    up

---
+++ SpaceHSG/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpaceHSG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
